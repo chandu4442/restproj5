27bb45877d42d9229537a9a373295f93e56a0915
chandu4442 <43944925+chandu4442@users.noreply.github.com>
2018-11-29 15:58:53 +0530

    Update models.py

--- dbapp5/models.py
+++ dbapp5/models.py
@@ -4,7 +4,7 @@ from django.db import models
 from django.db import models
 class Product(models.Model):
     pid=models.IntegerField(primary_key=True)
-    pname=models.CharField(max_length=20)
+    pname=models.CharField(max_length=30)
     pcost=models.DecimalField(max_digits=10,
                               decimal_places=4)
     pmfd=models.DateField()
